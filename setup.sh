00e0be2e8d68f7cbc64e40e0ca04b93675c3a6b3
Ross Baquir <streakinthesky@gmail.com>
2021-11-24 22:44:44 -0800

    create zfunc dir and and completions

--- setup.sh
+++ setup.sh
@@ -15,14 +15,18 @@ git clone https://github.com/StreakInTheSky/dotfiles.git ~/.dotfiles
 echo "\nInstalling zsh plugins with antibody"
 antibody bundle < ~/.dotfiles/zsh_plugins.txt > ~/.zsh_plugins.sh
 
+echo "\nCreating .zfunc directory"
+mkdir ~/.zfunc
+
 echo "\nConfiguring .zshrc with defaults"
 echo "source ~/.dotfiles/zshrc_defaults" >> ~/.zshrc
-#zsh -c "source ~/.zshrc"
+zsh -c "source ~/.zshrc"
 
 echo "\nInstalling python"
 git clone https://github.com/momo-lab/pyenv-install-latest.git "$(pyenv root)"/plugins/pyenv-install-latest
 pyenv install-latest
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+poetry completions zsh > ~/.zfunc/_poetry
 
 echo "\nInstalling rust" 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
